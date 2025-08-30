ce5de3c823760a4039864b9fb1a89455f5ded3e9
Mudkipalpharius <ekohlmeyer2022@ash.nl>
2025-08-30 19:23:02 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,55 +1,41 @@
 import java.util.Scanner;
-class Main {
-  public static void main(String[] args) {
-    Scanner cat = new Scanner(System.in);
-    System.out.println("caluculater asks what do you want to do");
-    System.out.println("1 add");
-    System.out.println("2 subtract");
-    System.out.println("3 multiply");
-    System.out.println("4 divide");
-    System.out.println("5 shell shockers");
 
-    int number = cat.nextInt();
-    if (number == 5) {
-       System.out.println("https://shellshock.io");
-    }
-
-    if (number == 1) {
-      System.out.println("1st number: ");
-      double num1 = cat.nextInt();
-      System.out.println("2nd number:");
-      double num2 = cat.nextInt();
-
-      double result = num1 + num2;
-      System.out.println("result: " + num1 + " + " + num2 + " = " + result);
-    }
-
-    if (number == 2) {
-      System.out.println("1st number: ");
-      double num3 = cat.nextInt();
-      System.out.println("2nd number:");
-      double num4 = cat.nextInt();
-
-      double result2 = num3 - num4;
-      System.out.println("result: " + num3 + " - " + num4 + " = " + result2);
-    }
-    if (number == 3) {
-      System.out.println("1st number: ");
-      double num5 = cat.nextInt();
-      System.out.println("2nd number:");
-      double num6 = cat.nextInt();
-
-      double result3 = num5 * num6;
-      System.out.println("result: " + num5 + " * " + num6 + " = " + result3);
-  }
-  if (number == 4) {
-      System.out.println("1st number: ");
-      double num7 = cat.nextInt();
-      System.out.println("2nd number:");
-      double num8 = cat.nextInt();
-
-      double result4 = num7 / num8;
-      System.out.println("result: " + num7 + " / " + num8 + " = " + result4);
-  }
-}
-}
+public class Main
+{
+    public static void main(String[] args)
+    {
+       
+        System.out.println("intput amount of coffees");
+        Scanner scan1 = new Scanner(System.in);
+        int coffeeAmount = scan1.nextInt();
+        
+        System.out.println("intput amount of pastries");
+        Scanner scan2 = new Scanner(System.in);
+        int pastryAmount = scan2.nextInt();
+       
+        System.out.println("intput amount of teas");
+        Scanner scan3 = new Scanner(System.in);
+        int teaAmount = scan3.nextInt();
+        System.out.println("Please type in a promo code");
+        Scanner scan = new Scanner(System.in);
+        String code = scan.nextLine();
+        double coffeePrice=2.10;
+        double teaPrice=1.60;
+        double pastryPrice=2.75;
+        if (code.equals("STUDENT10"))
+        {
+            coffeePrice=2.10*0.9;
+            teaPrice=1.60*0.9;
+            pastryPrice=2.75*0.9;
+        }
+        
+        
+        double subtotal = (((coffeeAmount*coffeePrice)+(pastryAmount*pastryPrice)+(teaAmount*teaPrice))*1.21)*1.3;
+        System.out.println("coffee   amount: "+coffeeAmount+" price "+((double)Math.round(coffeePrice*coffeeAmount*100))/100);
+        System.out.println("pastry  amount: "+pastryAmount+" price "+((double)Math.round(pastryPrice*pastryAmount*100))/100);
+        System.out.println("tea   amount: "+teaAmount+" price "+((double)Math.round(teaPrice*teaAmount*100))/100);
+        System.out.println("tax "+(((double)Math.round(((teaPrice*teaAmount*100)+(coffeePrice*coffeeAmount*100)+(pastryPrice*pastryAmount*100))*0.21))/100));
+        System.out.println("service fee "+(((double)Math.round(((teaPrice*teaAmount*100)+(coffeePrice*coffeeAmount*100)+(pastryPrice*pastryAmount*100))*1.21*0.3))/100));
+        System.out.println("total "+ subtotal);
+     }
+ }
